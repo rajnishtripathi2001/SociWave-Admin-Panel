bbc750b7164b8d786d58e90a2ae7d938487b9480
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2024-02-01 00:06:43 +0530

    Add wallet status functionality

--- src/Pages/Wallets.jsx
+++ src/Pages/Wallets.jsx
@@ -9,6 +9,7 @@ export default function Wallets() {
   const [moneyScreen, setMoneyScreen] = useState(false);
   const [transactionScreen, setTransactionScreen] = useState(false);
   const [walletID, setWalletID] = useState("");
+  const [walletStatus, setWalletStatus] = useState(false);
 
   const handleMoneyScreen = () => {
     setMoneyScreen(true);
@@ -20,32 +21,36 @@ export default function Wallets() {
     setTransactionScreen(true);
   };
 
-
   const [userData, setUserData] = useState({
     id: "",
     status: "",
     balance: "",
     spending: "",
-    lastTrans:"",
-    lastDate:"",
+    lastTrans: "",
+    lastDate: "",
   });
 
   const findWallet = () => {
+    if (walletID === "") {
+      alert("Please Enter the wallet ID");
+    }
     axios
       .post("https://flask-backend.up.railway.app/findWallet", { walletID })
       .then((response) => {
         if (response.data.message === "Found") {
           setUserData({
-            id:response.data.result._id,
-            status:response.data.result.walletstatus,
-            balance:response.data.result.balance,
-            spending:response.data.result.spending,
-            lastTrans:response.data.result.lastTransaction,
-            lastDate:response.data.result.lastTransactionDate
-          })
-        }
-        else{
-          alert(response.data.message)
+            id: response.data.result._id,
+            status: response.data.result.walletstatus,
+            balance: response.data.result.balance,
+            spending: response.data.result.spending,
+            lastTrans: response.data.result.lastTransaction,
+            lastDate: response.data.result.lastTransactionDate,
+          });
+          setWalletStatus(true);
+        } else {
+          alert(response.data.message);
+          setUserData("");
+          setWalletStatus(false);
         }
       })
       .catch((err) => {
@@ -54,23 +59,24 @@ export default function Wallets() {
   };
 
   let addMoneyProp = {
-    id:walletID,
-    balance:userData.balance
-  }
+    id: walletID,
+    balance: userData.balance,
+  };
 
-  const walletState = (command)=>{
+  const walletState = (command) => {
     let task = {
-      id:walletID,
-      state:command
-    }
-    axios.post("https://flask-backend.up.railway.app/walletState",{task})
-    .then((res)=>{
-      alert("Wallet Status Updated")
-        })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
+      id: walletID,
+      state: command,
+    };
+    axios
+      .post("https://flask-backend.up.railway.app/walletState", { task })
+      .then((res) => {
+        alert("Wallet Status Updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="flex">
@@ -101,15 +107,18 @@ export default function Wallets() {
                   ID : <span className="text-blue-500">{userData.id}</span>
                 </td>
                 <td className="border-2 w-1/2 p-1 align-top">
-                  Status : <span className="text-blue-500">{userData.status}</span>
+                  Status :{" "}
+                  <span className="text-blue-500">{userData.status}</span>
                 </td>
               </tr>
               <tr>
                 <td className="border-2 w-1/2 p-1 align-top">
-                  Balance : <span className="text-blue-500">{userData.balance}</span>
+                  Balance :{" "}
+                  <span className="text-blue-500">{userData.balance}</span>
                 </td>
                 <td className="border-2 w-1/2 p-1 align-top">
-                  Spending : <span className="text-blue-500">{userData.spending}</span>
+                  Spending :{" "}
+                  <span className="text-blue-500">{userData.spending}</span>
                 </td>
               </tr>
               <tr>
@@ -126,27 +135,55 @@ export default function Wallets() {
 
             <div className="mt-7 flex">
               <div className="w-4/5 h-[55vh]">
-                {moneyScreen ? <AddMoney  {...addMoneyProp} /> : null}
-                {transactionScreen ? <Transactions walletID={walletID} /> : null}
+                {moneyScreen ? <AddMoney {...addMoneyProp} /> : null}
+                {transactionScreen ? (
+                  <Transactions walletID={walletID} />
+                ) : null}
               </div>
               <div className="content-none h-[100] w-[0.9px] ml-5 bg-gray-600"></div>
               <div className="w-1/5 h-[55vh] px-5">
+                {walletStatus ? (
+                  <>
+                    <button
+                      className="bg-blue-500 text-white px-4 py-2 w-full rounded-md my-1"
+                      onClick={handleMoneyScreen}
+                    >
+                      Add Money
+                    </button>
+                    <button
+                      className="bg-blue-500 text-white px-4 py-2 w-full rounded-md my-1"
+                      onClick={handleTransactionScreen}
+                    >
+                      See All Transactions
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      className="bg-gray-500 text-white line-through px-4 py-2 w-full rounded-md my-1"
+                      disabled
+                    >
+                      Add Money
+                    </button>
+                    <button
+                      className="bg-gray-500 text-white line-through px-4 py-2 w-full rounded-md my-1"
+                      disabled
+                    >
+                      See All Transactions
+                    </button>
+                  </>
+                )}
+
                 <button
-                  className="bg-blue-500 text-white px-4 py-2 w-full rounded-md my-1"
-                  onClick={handleMoneyScreen}
+                  className="bg-green-500 text-white px-4 py-2 w-full rounded-md my-1"
+                  onClick={() => walletState("Active")}
                 >
-                  Add Money
-                </button>
-                <button
-                  className="bg-blue-500 text-white px-4 py-2 w-full rounded-md my-1"
-                  onClick={handleTransactionScreen}
-                >
-                  See All Transactions
-                </button>
-                <button className="bg-green-500 text-white px-4 py-2 w-full rounded-md my-1" onClick={()=>walletState("Active")}>
                   Activate Wallet
                 </button>
-                <button className="bg-red-500 text-white px-4 py-2 w-full rounded-md my-1" onClick={()=>walletState("Seized")}>
+                <button
+                  className="bg-red-500 text-white px-4 py-2 w-full rounded-md my-1"
+                  onClick={() => walletState("Seized")}
+                >
                   Seize Wallet
                 </button>
               </div>
