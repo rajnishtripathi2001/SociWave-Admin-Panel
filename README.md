28e68a4f121ebd70a28a57ea5cc544163a290d5c
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2024-02-20 22:36:34 +0530

    Refactor maintenance page and add API calls

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Getting Started
 This Admin Panel is built using ReactJS and TailwindCSS. It is a part of the Sociwave project. It is used to manage the Sociwave project.
 Its backend is built using Flask and MongoDB. It is hosted on Railway. The backend repository can be found [Backend-Repo](https://github.com/rajnishtripathi2001/flask-backend).
+
 This Admin Panel is designed to be used by the Admins of Sociwave. It has the following features:
 - Manage Users
 - Manage Wallets
@@ -11,5 +12,3 @@ This Admin Panel is designed to be used by the Admins of Sociwave. It has the fo
 - Manage Sociwave Feedbacks
 - Manage Sociwave Suggestions
 - Manage Sociwave Complaints and Queries
-
-
--- src/Pages/Maintenance.jsx
+++ src/Pages/Maintenance.jsx
@@ -1,53 +1,125 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../Comonents/Sidebar";
 import MainScreen from "../Comonents/MainScreen";
+import axios from "axios";
 
 export default function Maintenance() {
+  const [mode, setMode] = useState();
 
-  const [mode,setMode] = useState(true)
+  const [status, setStatus] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://flask-backend.up.railway.app/getMaintenance")
+      .then((res) => {
+        if (res.data.Result.maintinanceMode === "Active") {
+          setMode(1);
+          setStatus("Website is Active and Running Fine.");
+        } else if (res.data.Result.maintinanceMode === "Under Maintenance") {
+          setMode(0);
+          setStatus("Website is Under Maintenance and will be back soon.");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const startMaintenance = async () => {
+    await axios
+      .post("https://flask-backend.up.railway.app/maintenance", { command: "start" })
+      .then((res) => {
+        setMode(1);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const stopMaintenance = async () => {
+    await axios
+      .post("https://flask-backend.up.railway.app/maintenance", { command: "stop" })
+      .then((res) => {
+        setMode(0);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="flex">
       <Sidebar />
       <MainScreen>
         <div className="text-white flex flex-col items-center m-5">
-          Maintenance Page
-          {mode ? (
-            <div className="border w-full my-5 p-2 h-[85vh]">
-           
-            <div className="flex flex-col items-center my-5">
-              <img
-                width="94"
-                height="94"
-                src="https://img.icons8.com/3d-fluency/94/hard-working.png"
-                alt="hard-working"
-              />   
-            </div>
-            Current Mode : <span className="text-green-500">Active</span>
-            <div className="flex flex-col items-center my-5">
-              <textarea name="message" id="message" cols="100" rows="5" placeholder="Enter the maintenance message"></textarea>  
-              <div className="flex my-2">
-              <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">
-                Start Maintenance
-              </button>
-              <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">
-                Stop Maintenance
-              </button>
-              </div>
+          <h1 className="text-3xl font-bold"> Maintenance Page</h1>
 
-            </div>
+          <div className="border w-full my-5 p-2 h-[85vh] text-center">
+            {mode ? (
+              <>
+                <div className="flex flex-col items-center my-5 text-center">
+                  <img
+                    width="94"
+                    height="94"
+                    src="https://img.icons8.com/3d-fluency/94/hard-working.png"
+                    alt="hard-working"
+                  />
+                </div>
+                <span className="text-green-500">{status}</span>
+                <div className="flex flex-col items-center my-5">
+                  <div className="flex my-2">
+                    <button
+                      type="button"
+                      class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+                      onClick={startMaintenance}
+                    >
+                      Start Maintenance
+                    </button>
+                    <button
+                      type="button"
+                      class="text-white bg-gray-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+                      // onClick={stopMaintenance}
+                      disabled
+                    >
+                      Stop Maintenance
+                    </button>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex flex-col items-center my-5 text-center">
+                  <img
+                    width="94"
+                    height="94"
+                    src="https://img.icons8.com/external-bearicons-blue-bearicons/64/external-Under-Maintenance-website-bearicons-blue-bearicons.png"
+                    alt="Under-Maintenance-website"
+                  />
+                </div>
+                <span className="text-red-500">{status}</span>
+                <div className="flex flex-col items-center my-5">
+                  <div className="flex my-2">
+                    <button
+                      type="button"
+                      class="text-white bg-gray-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+                      disabled
+                    >
+                      Start Maintenance
+                    </button>
+                    <button
+                      type="button"
+                      class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+                      onClick={stopMaintenance}
+                    >
+                      Stop Maintenance
+                    </button>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
-          ) : (
-            <p>
-            unser mentinance
-            <img
-                width="94"
-                height="94"
-                src="https://img.icons8.com/external-sapphire-kerismaker/48/external-Maintenence-seo-sem-sapphire-kerismaker.png"
-                alt="external-Maintenence-seo-sem-sapphire-kerismaker"
-              />
-          </p>)}
-          
         </div>
       </MainScreen>
     </div>
